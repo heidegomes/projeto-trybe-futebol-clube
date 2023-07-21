d2854d0ef8220c72130c6703720f994549e06d82
heidegomes <heidegc@hotmail.com>
2023-07-21 15:59:20 -0300

    requisito 24 e 25 feito!

--- app/backend/src/database/models/queryAway.ts
+++ app/backend/src/database/models/queryAway.ts
@@ -1,4 +1,4 @@
-const QUERY = `select *, round(totalPoints/(totalGames * 3)*100, 2) as efficiency
+const QUERY = `select *, round(totalPoints/(totalGames * 3)*100, 2) as efficiencfy
 from (
     SELECT team_name as name, ((sum(totalVictories) * 3) + (sum(totalDraws) * 1))
     as totalPoints, 
@@ -18,6 +18,18 @@ from (
         inner join matches m on m.home_team_id = t.id 
         where m.in_progress = 0
           order by t.id
+        )
+        UNION 
+        (
+        select t.id, t.team_name, m.away_team_goals as goalsFavor,
+        m.home_team_goals as goalsOwn,
+          if (m.home_team_goals < m.away_team_goals, 1, 0) as totalVictories,
+          if (m.home_team_goals = m.away_team_goals, 1, 0) as totalDraws,
+          if (m.home_team_goals > m.away_team_goals, 1, 0) as totalLosses
+        from teams t
+        inner join matches m on m.away_team_id = t.id 
+        where m.in_progress = 0
+        order by t.id
         )
     ) as tj
     group by id, team_name
