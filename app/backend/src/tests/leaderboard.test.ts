fa6549c406ed3813620abd87b2642e943c9f5d30
heidegomes <heidegc@hotmail.com>
2023-07-24 16:46:04 -0300

    fluxo 4 finalizado!

--- app/backend/src/tests/leaderboard.test.ts
+++ app/backend/src/tests/leaderboard.test.ts
@@ -1,17 +1,11 @@
 import * as sinon from 'sinon';
 import * as chai from 'chai';
-import * as jwt from 'jsonwebtoken';
 import sequelize from '../database/models/index';
 // @ts-ignore
 import chaiHttp = require('chai-http');
 
 import { App } from '../app';
 
-import MatchesModel from '../database/models/MatchesModel';
-import { match41, matches, matchesFinished, matchesInProgress, nonExistentTeam, reqNewMatch, resNewMatch, resultUpdated, teamsModelById, twoSameTeams } from './mocks/Matches.mock';
-import TeamsModel from '../database/models/TeamsModel';
-import { send } from 'process';
-import { match } from 'assert';
 import { leaderBoardAway, leaderBoardGeneral, leaderBoardHome } from './mocks/Leaderboard.mock'
 
 chai.use(chaiHttp);
